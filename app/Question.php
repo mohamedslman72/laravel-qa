0e754e91a147146b79cb5e747203906b5c6e00ec
Mohamed <mohamed.slman72@gmail.com>
2020-06-26 22:20:19 +0200

    Generate Question model via artisan modle generator

---
+++ app/Question.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Question extends Model
+{
+    use SoftDeletes;
+    protected $fillable = ['title','body'];
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+    public function setTitleAttribute($value)
+    {
+        $this->attributes['title']= $value;
+        $this->attributes['slug']= str_slug($value);
+    }
+}
